b8cc530169f622f9151f3dea9ac5cd01dbe02fd3
Ulises Schreiner <ulisesschreiner@Ulisess-MacBook-Pro.local>
2024-08-18 18:51:39 -0300

    node update 02

--- lib/ui/event/WebRedirectionEvent.ts
+++ lib/ui/event/WebRedirectionEvent.ts
@@ -3,9 +3,11 @@ import { StepEvent } from '../../api/contract/StepEvent';
 export class WebRedirectionEvent extends StepEvent {
   type = "web_redirection_event";
   path: string = "";
+  queryParams: Record<string, string | number> = {}
 
-  constructor(path: string) {
+  constructor(path: string, queryParams: Record<string, string | number>) {
     super();
     this.path = path;
+    this.queryParams = queryParams
   }
 }
