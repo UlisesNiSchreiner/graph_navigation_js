c02cb9ee012a727b556fadd2233a86ff4ad4dd74
UlisesNiSchreiner <ulises.schreiner.dev@gmail.com>
2024-08-19 00:53:13 -0300

    output to data

--- lib/api/contract/RequestData.ts
+++ lib/api/contract/RequestData.ts
@@ -3,26 +3,26 @@ export interface RequestPayloadData {
   queryParams?: Record<string, any>;
 }
 export class RequestData {
-    token?: string;
-    session?: string;
-    data?: RequestPayloadData = {output: {}};
-    navigation?: string[];
-    nextStep?: string;
-    private static _instance: RequestData | null = null;
+  token?: string;
+  session?: string;
+  data?: RequestPayloadData = { output: {} };
+  navigation?: string[];
+  nextStep?: string;
+  private static _instance: RequestData | null = null;
 
-   getData = <T>(key: string): T | null => {
-      const output = (this.data as { output: Record<string, any> })?.output;
-      if (output === undefined || output === null || !(key in output)) {
-        return null;
-      }
-      return output[key] as T;
+  getData = <T>(key: string): T | null => {
+    const output = (this.data as { output: Record<string, any> })?.output;
+    if (output === undefined || output === null || !(key in output)) {
+      return null;
     }
-
-    static TODO() {
-      if (!RequestData._instance) {
-        RequestData._instance = new RequestData();
-      }
-      RequestData._instance.data = {output: {}}
-      return RequestData._instance;
-    }    
+    return output[key] as T;
   }
+
+  static TODO() {
+    if (!RequestData._instance) {
+      RequestData._instance = new RequestData();
+    }
+    RequestData._instance.data = { output: {} }
+    return RequestData._instance;
+  }
+}
