33d15f58cd070a8508e35e875af6eb81af524eca
UlisesNiSchreiner <ulises.schreiner.dev@gmail.com>
2024-08-26 22:55:22 -0300

    add custom event

--- lib/ui/components/common/button/BlockButton.ts
+++ lib/ui/components/common/button/BlockButton.ts
@@ -1,11 +1,14 @@
 import { Spacing } from "../style";
 import { Component } from '../../../../api/contract/Component';
+import { StepEvent } from "lib/api";
 
 export class BlockButton extends Component {
   uiType = "block_button";
   backgroundColor?: string = "#ffffff";
   color?: string = "#2e86c1";
   hoverColor?: string = "#aed6f1";
+  enableObserverEvent?: StepEvent;
+  disableObserverEvent?: StepEvent;
   disableOnFetching?: true;
   text?: string = "";
   spacing?: Spacing = {
@@ -14,4 +17,5 @@ export class BlockButton extends Component {
     right: 5,
     bottom: 5,
   };
+  initialDisabled: boolean = false;
 }
